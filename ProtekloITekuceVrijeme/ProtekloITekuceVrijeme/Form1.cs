c569f97d7acc2aa7267e7f3bf900cc2236ac889e
Neven <58472164+neven14@users.noreply.github.com>
2021-10-10 17:24:56 +0200

    Add files via upload

---
+++ ProtekloITekuceVrijeme/ProtekloITekuceVrijeme/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProtekloITekuceVrijeme
+{
+    public partial class Form1 : Form
+    {
+        DateTime pocetno;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = DateTime.Now - pocetno;
+            toolStripStatusLabel1.Text = "Proteklo: " +
+            proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime pocetno = DateTime.Now;
+        }
+    }
+}
